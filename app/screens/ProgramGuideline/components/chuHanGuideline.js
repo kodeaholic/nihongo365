bc556e3a2ff5891a5c0b162f51da580edbf9b996
seta-nathan <nnguyen4@veritone.com>
2021-06-23 18:37:04 +0700

    Done chuhan guide

--- app/screens/ProgramGuideline/components/chuHanGuideline.js
+++ app/screens/ProgramGuideline/components/chuHanGuideline.js
@@ -13,74 +13,100 @@ function ProgramInfo({ theme }) {
   const program = programs.find(itx => itx.id === selectedID);
   return (
     <React.Fragment>
-      <Text style={styles.title}>Hướng dẫn học</Text>
-      <Card style={styles.card}>
-        <React.Fragment>
-          <View style={styles.content}>
-            <Paragraph style={styles.paragraph}>
-              Từ vựng trong tiếng Nhật rất nhiều, học được càng nhiều từ thì lại
-              càng tốt. Nhưng làm thế nào để học được nhiều từ vựng đây? Tại sao
-              học toàn quên? Hay là học thuộc từ vựng nhưng không biết vận dụng
-              từ đó để hội thoại?
-            </Paragraph>
-            <Paragraph style={styles.paragraph}>
-              Hiểu được nỗi băn khoăn của các bạn, Ad đã tổng hợp cách học từ
-              vựng hiệu quả nhất. Các bạn nên áp dụng trong việc chinh phục từ
-              vựng tiếng Nhật của mình nhé.
-            </Paragraph>
-            <Section
-              name="Học từ vựng theo hệ thống, theo chủ đề"
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Mỗi từ vựng phải có ít nhất 1 ví dụ minh họa để làm nổi bật từ vựng đó, đồng thời các bạn cũng hiểu được từ vựng đó sử dụng trong những trường hợp nào."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Những từ vựng đã học hãy cố gắng sử dụng để nói chuyện với người Nhật, nếu cách sử dụng không đúng thì người Nhật sẽ tự động sửa giúp mình."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Chỉ nên quy định trong 1 tuần học từ vựng của 1 chủ đề, luôn sử dụng từ vựng chủ đề đó trong hội thoại tuần đó."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Cuối tuần viết 1 bài văn ngắn tầm 10 dòng, sử dụng tối đa từ vựng trong chủ đề đó để viết văn."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Sau 1 tháng ôn tổng hợp những gì đã học, nhờ bạn bè check từ vựng với tần suất nhanh dần để luyện phản xạ, ghi chép lại những từ mình phản ứng chậm và chưa nhớ để học lại."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-            <Section
-              name="Thường xuyên luyện những bài tập về phần từ vựng, Ad cũng đã tổng hợp luyện đề theo từ vựng riêng."
-              icon="check"
-              color={colors.accent}
-              style={styles.section}
-            />
-          </View>
-          <AvatarSignature />
-        </React.Fragment>
-      </Card>
+      <View style={{ backgroundColor: '#e5dfd7' }}>
+        <Text style={styles.title}>Hướng dẫn học</Text>
+        <Card style={styles.card}>
+          <React.Fragment>
+            <View style={styles.content}>
+              <View style={styles.paragraph}>
+                <Text
+                  style={[
+                    styles.paragraph,
+                    { fontWeight: 'bold', color: '#5cdb5e' },
+                  ]}>
+                  Âm On
+                </Text>
+                <Text style={styles.paragraph}>
+                  {'    - '}Là âm Hán Nhật, thường thì từ 2 chữ hán trở lên thì
+                  sử dụng âm On ghép với nhau để đọc. Nhiều trường hợp đặc biệt
+                  thì 2 chữ hán cũng có thể sử dụng âm Kun, âm On ghép lẫn nhau
+                  để đọc. Chữ nào có trường hợp đó thì Ad sẽ đưa ra ở phần ví
+                  dụ.
+                </Text>
+              </View>
+              <View style={styles.paragraph}>
+                <Text
+                  style={[
+                    styles.paragraph,
+                    { fontWeight: 'bold', color: '#5cdb5e' },
+                  ]}>
+                  Âm Kun
+                </Text>
+                <Text style={styles.paragraph}>
+                  {'    - '}Là âm thuần Nhật, thường là sử dụng để đọc động từ,
+                  1 chữ hán, các trợ từ...
+                </Text>
+              </View>
+              <Section
+                name="Để có thể đọc được chữ Hán thì ta phải nhớ được âm On và âm Kun."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Để có thể viết được thì phải nhớ thành phần tạo nên chữ đó. Ví như trong chữ Hưu ( 休) thì bao gồm Nhân đứng (亻)  và  bộ Mộc (木). Bạn không phải lo lắng phải tìm từng bộ ở đâu cả. Khi học đến chữ Hán nào thì Ad sẽ thêm thành phần của chữ  đó."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Không cần phải học quy tắc viết ( Từ trong ra ngoài, trên rồi dưới...) các bạn chỉ cần viết theo thứ tự các nét mà chữ đó hiển thị, sau thời gian tay của bạn sẽ tự quen cách viết đó."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Không phải thống kê và học ngấu nghiến để hết các bộ thủ ấy rồi mới học chữ Hán. Học đến cuối thì đoạn đầu nhớ bao nhiêu chữ. Ad đã chỉ ra bộ thủ trong từng bài, từng chữ Hán đó."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Ví dụ minh họa cho chữ Hán là từ vựng thường hay sử dụng và thiết thực. Các bạn nên nhớ đa số từ vựng đó và khi học xong phần chữ Hán thì vốn từ vựng khá nhiều rồi."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Cố gắng làm hết bài tập trắc nghiệm sau mỗi bài, khi học bài mới thì hãy 1 lần nữa ôn lại bài cũ. Sau 10 ngày thì ôn lại, sau 1 tháng lại ôn tổng hợp."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Cố gắng nhớ âm Hán Việt và nghĩa của nó, hiểu nó thì các bạn có thể tự ghép được từ vựng mới."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+              <Section
+                name="Quy định mỗi 2 ngày chỉ học 10 chữ Hán mà Ad đã soạn trong 1 bài. Chậm nhưng chắc, 2 ngày 10 từ, 20 ngày chắc chắn có 100 từ. Chữ Hán từ N5 đến N1 chỉ có gần 2000 từ thôi. Các bạn thử tính giúp Ad là bao nhiêu lâu học xong chữ Hán từ N5 đến N1 nhé. Tuy nhiên nếu không ôn lại nhiều lần thì sẽ bị quên."
+                icon="check"
+                color={colors.accent}
+                style={styles.section}
+              />
+            </View>
+            <AvatarSignature text={'Chúc bạn sớm nhớ nhanh chữ Hán!'} />
+          </React.Fragment>
+        </Card>
+      </View>
     </React.Fragment>
   );
 }
 
 const styles = StyleSheet.create({
   card: { borderRadius: 4 },
-  content: { padding: 16 },
+  content: { padding: 16, backgroundColor: '#e5dfd7' },
   title: {
     fontSize: 18,
     fontWeight: 'bold',

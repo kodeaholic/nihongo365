c41c7dc90b2549f7abb6ddf7b5f626ea92368610
seta-nathan <nnguyen4@veritone.com>
2021-09-02 17:30:22 -0700

    news: add categories modal

--- app/screens/News/index.js
+++ app/screens/News/index.js
@@ -9,7 +9,9 @@ import {
   TouchableOpacity,
   FlatList,
   ScrollView,
+  Button,
 } from 'react-native';
+import Modal from 'react-native-modal';
 import { Badge } from 'react-native-paper';
 import Skeleton from '@thevsstech/react-native-skeleton';
 import _ from 'lodash';
@@ -17,356 +19,167 @@ import { Dimensions } from 'react-native';
 import { apiConfig } from '../../api/config/apiConfig';
 import { authHeader } from '../../api/authHeader';
 // import DebounceInput from '../../components/DebounceInput';
-export default function News({ navigation }) {
+
+const News = ({ navigation }) => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [treeModalVisible, setTreeModalVisible] = useState(false);
+  const [headerProps, setHeaderProps] = useState({
+    title: 'Bảng tin',
+    disableBackButton: true,
+    leftAction: {
+      color: '#fff',
+      icon: 'table-of-contents',
+      action: () => setTreeModalVisible(true),
+    },
+  });
   useEffect(() => {
-    navigation.setOptions({ headerProps: { title: 'Bảng tin' } });
-  }, [navigation]);
-  const [keyWord, setKeyWord] = useState('');
-  const [searching, setSearching] = useState(false);
-  const [results, setResults] = useState([]);
-  const [selected, setSelected] = useState('dictionary');
-  useEffect(() => {
-    function clean(object) {
-      const obj = Object.assign({}, object);
-      const result = {};
-      for (var propName in obj) {
-        if (obj[propName].length > 0) {
-          result[propName] = obj[propName];
-        }
-      }
-      return result;
-    }
-    async function search(key) {
-      const headers = await authHeader();
-      const requestOptions = {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify({ search: key }),
-      };
-      let url = `${apiConfig.baseUrl}${apiConfig.apiEndpoint}/search`;
-      try {
-        setSearching(true);
-        const response = await fetch(url, requestOptions);
-        const data = await response.json();
-        if (data.code) {
-          setSearching(false);
-          ToastAndroid.showWithGravityAndOffset(
-            'Có lỗi tìm kiếm. Vui lòng thử lại sau ít phút',
-            ToastAndroid.SHORT,
-            ToastAndroid.TOP,
-            0,
-            100,
-          );
-        } else {
-          //   console.log(htmlEntityDecode(data.quiz[0].question));
-          let res = clean(data.results);
-          /* Filter for empty */
-          if (_.isEmpty(res)) {
-            setSearching(false);
-            ToastAndroid.showWithGravityAndOffset(
-              'Không tìm thấy kết quả tương tự',
-              ToastAndroid.LONG,
-              ToastAndroid.TOP,
-              0,
-              100,
-            );
-          } else {
-            setResults(res);
-            setSearching(false);
-            if (res.dictionary) {
-              setSelected('dictionary');
-            } else if (res.vocabs) {
-              setSelected('vocabs');
-            } else {
-              setSelected('cards');
-            }
-          }
-        }
-      } catch (error) {
-        setSearching(false);
-        return error;
-      }
-    }
-    if (keyWord.length) {
-      // console.log(keyWord);
-      search(keyWord);
-    } else {
-      setSearching(false);
-      setResults({});
-    }
-  }, [keyWord]);
+    navigation.setOptions({
+      headerProps: headerProps,
+    });
+  }, [navigation, headerProps]);
   const windowWidth = Dimensions.get('window').width;
-  const getItemLabel = item => {
-    switch (selected) {
-      case 'dictionary':
-        return item.phrase;
-      case 'vocabs':
-        return item.extractedVocab;
-      case 'cards':
-        return item.letter;
-      default:
-        return '';
-    }
-  };
-  const getItemMeaning = item => {
-    switch (selected) {
-      case 'dictionary':
-        return item.meaning;
-      case 'vocabs':
-        return item.vocabMeaning;
-      case 'cards':
-        return item.meaning;
-      default:
-        return '';
-    }
-  };
-  const renderListItem = (item, index) => {
-    return (
-      <View style={styles.listItem} key={item.id}>
-        <Text>{getItemLabel(item)}</Text>
-      </View>
-    );
-  };
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: '#FFFFFF', paddingBottom: 15 }}>
-      <View styles={styles.container}>
-        {searching && (
-          <Skeleton speed={1000}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFFFFF' }}>
+      <Modal
+        isVisible={treeModalVisible}
+        animationIn="bounceInLeft"
+        animationOut="bounceOutLeft"
+        animationInTiming={1500}
+        animationOutTiming={2000}
+        onBackButtonPress={() => setTreeModalVisible(false)}
+        onBackdropPress={() => setTreeModalVisible(false)}>
+        <View style={{ flex: 1 }}>
+          <Text
+            style={[
+              {
+                fontFamily: 'SF-Pro-Display-Regular',
+                textAlign: 'center',
+                color: '#fff',
+                fontWeight: '500',
+                fontSize: 16,
+                textTransform: 'uppercase',
+              },
+            ]}>
+            Chuyên mục
+          </Text>
+        </View>
+      </Modal>
+      {/* <View style={styles.container}>
+        <Skeleton speed={1000}>
+          <View
+            style={[
+              styles.skeletonRow,
+              {
+                marginLeft: 8,
+                marginRight: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 36,
+              },
+            ]}>
             <View
               style={[
-                styles.skeletonRow,
                 {
-                  marginLeft: 8,
-                  marginRight: 8,
-                  flexDirection: 'row',
-                  alignItems: 'center',
+                  width: (windowWidth - 32) / 3,
                   height: 36,
-                },
-              ]}>
-              <View
-                style={[
-                  {
-                    width: (windowWidth - 32) / 3,
-                    height: 36,
-                    borderRadius: 50,
-                    marginRight: 10,
-                  },
-                ]}
-              />
-              <View
-                style={[
-                  {
-                    width: (windowWidth - 32) / 3,
-                    height: 36,
-                    borderRadius: 50,
-                    marginRight: 10,
-                  },
-                ]}
-              />
-              <View
-                style={[
-                  {
-                    width: (windowWidth - 32) / 3,
-                    height: 36,
-                    borderRadius: 50,
-                    marginRight: 0,
-                  },
-                ]}
-              />
-            </View>
-            <View
-              style={[
-                styles.skeletonRow,
-                {
-                  marginTop: 10,
-                  marginLeft: 8,
-                  marginRight: 8,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  height: 80,
-                  width: windowWidth - 16,
-                  borderRadius: 10,
+                  borderRadius: 50,
+                  marginRight: 10,
                 },
               ]}
             />
             <View
               style={[
-                styles.skeletonRow,
                 {
-                  marginTop: 10,
-                  marginLeft: 8,
-                  marginRight: 8,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  height: 80,
-                  width: windowWidth - 16,
-                  borderRadius: 10,
+                  width: (windowWidth - 32) / 3,
+                  height: 36,
+                  borderRadius: 50,
+                  marginRight: 10,
                 },
               ]}
             />
             <View
               style={[
-                styles.skeletonRow,
                 {
-                  marginTop: 10,
-                  marginLeft: 8,
-                  marginRight: 8,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  height: 80,
-                  width: windowWidth - 16,
-                  borderRadius: 10,
+                  width: (windowWidth - 32) / 3,
+                  height: 36,
+                  borderRadius: 50,
+                  marginRight: 0,
                 },
               ]}
             />
-            <View
-              style={[
-                styles.skeletonRow,
-                {
-                  marginTop: 10,
-                  marginLeft: 8,
-                  marginRight: 8,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  height: 80,
-                  width: windowWidth - 16,
-                  borderRadius: 10,
-                },
-              ]}
-            />
-          </Skeleton>
-        )}
-        {!searching && !_.isEmpty(results) && (
-          <View style={styles.buttonGroup}>
-            {true && (
-              <TouchableOpacity
-                disabled={
-                  selected === 'dictionary' ||
-                  !_.get(results, 'dictionary.length')
-                }
-                onPress={() => setSelected('dictionary')}
-                style={[
-                  styles.buttonGroupItem,
-                  selected === 'dictionary' ? styles.buttonActive : {},
-                ]}>
-                <Text
-                  style={[
-                    styles.buttonText,
-                    selected === 'dictionary'
-                      ? styles.textActivie
-                      : styles.textNormal,
-                  ]}>
-                  Từ điển
-                </Text>
-                {_.get(results, 'dictionary.length') && (
-                  <Badge
-                    style={[
-                      selected === 'dictionary'
-                        ? styles.badgeActive
-                        : styles.badge,
-                    ]}>
-                    {_.get(results, 'dictionary.length')}
-                  </Badge>
-                )}
-              </TouchableOpacity>
-            )}
-            {true && (
-              <TouchableOpacity
-                onPress={() => setSelected('vocabs')}
-                disabled={
-                  selected === 'vocabs' || !_.get(results, 'vocabs.length')
-                }
-                style={[
-                  styles.buttonGroupItem,
-                  selected === 'vocabs' ? styles.buttonActive : {},
-                ]}>
-                <Text
-                  style={[
-                    styles.buttonText,
-                    selected === 'vocabs'
-                      ? styles.textActivie
-                      : styles.textNormal,
-                  ]}>
-                  Từ vựng
-                </Text>
-                {_.get(results, 'vocabs.length') && (
-                  <Badge
-                    style={[
-                      selected === 'vocabs' ? styles.badgeActive : styles.badge,
-                    ]}>
-                    {_.get(results, 'vocabs.length')}
-                  </Badge>
-                )}
-              </TouchableOpacity>
-            )}
-            {true && (
-              <TouchableOpacity
-                onPress={() => setSelected('cards')}
-                disabled={
-                  selected === 'cards' || !_.get(results, 'cards.length')
-                }
-                style={[
-                  styles.buttonGroupItem,
-                  styles.buttonGroupItem_lastChild,
-                  selected === 'cards' ? styles.buttonActive : {},
-                ]}>
-                <Text
-                  style={[
-                    styles.buttonText,
-                    selected === 'cards'
-                      ? styles.textActivie
-                      : styles.textNormal,
-                  ]}>
-                  Chữ Hán
-                </Text>
-                {_.get(results, 'cards.length') && (
-                  <Badge
-                    style={[
-                      selected === 'cards' ? styles.badgeActive : styles.badge,
-                    ]}>
-                    {_.get(results, 'cards.length')}
-                  </Badge>
-                )}
-              </TouchableOpacity>
-            )}
           </View>
-        )}
-        {!searching && !_.isEmpty(results[selected]) && (
-          <FlatList
-            data={results[selected]}
-            renderItem={({ item, index }) => {
-              return (
-                <View style={styles.listItem}>
-                  <Text style={[styles.japaneseText]}>
-                    {getItemLabel(item)}
-                  </Text>
-                  <Text style={[styles.japaneseText]}>
-                    {getItemMeaning(item)}
-                  </Text>
-                </View>
-              );
-            }}
-            keyExtractor={(item, index) => {
-              return item.id;
-            }}
+          <View
+            style={[
+              styles.skeletonRow,
+              {
+                marginTop: 10,
+                marginLeft: 8,
+                marginRight: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 80,
+                width: windowWidth - 16,
+                borderRadius: 10,
+              },
+            ]}
           />
-        )}
-      </View>
+          <View
+            style={[
+              styles.skeletonRow,
+              {
+                marginTop: 10,
+                marginLeft: 8,
+                marginRight: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 80,
+                width: windowWidth - 16,
+                borderRadius: 10,
+              },
+            ]}
+          />
+          <View
+            style={[
+              styles.skeletonRow,
+              {
+                marginTop: 10,
+                marginLeft: 8,
+                marginRight: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 80,
+                width: windowWidth - 16,
+                borderRadius: 10,
+              },
+            ]}
+          />
+          <View
+            style={[
+              styles.skeletonRow,
+              {
+                marginTop: 10,
+                marginLeft: 8,
+                marginRight: 8,
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 80,
+                width: windowWidth - 16,
+                borderRadius: 10,
+              },
+            ]}
+          />
+        </Skeleton>
+      </View> */}
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 5,
     flex: 1,
   },
-  skeletonRow: {
-    // marginLeft: 8,
-    // marginRight: 8,
-    // // paddingLeft: 8,
-    // // paddingRight: 8,
-  },
+  skeletonRow: {},
   buttonGroup: {
     flexDirection: 'row',
     alignContent: 'stretch',
@@ -447,3 +260,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+
+export default News;

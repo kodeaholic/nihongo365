0a36716edb578a975e84651167bc17785a199a0f
seta-nathan <nnguyen4@veritone.com>
2021-09-04 18:28:37 -0700

    chat: convert to use hooks

--- app/screens/Chat/index.js
+++ app/screens/Chat/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import {
 //   Platform,
 //   StyleSheet,
@@ -15,129 +15,113 @@ import { ChatScreen } from 'react-native-easy-chat-ui';
 import { v4 as uuidv4 } from 'uuid';
 import { isIphoneX } from '../../lib/isIphoneX';
 const isIPX = isIphoneX();
-export class Chat extends React.Component {
-  componentWillMount() {
-    console.log(this.props);
-  }
-  state = {
-    messages: [
-      {
-        id: uuidv4(),
-        type: 'text',
-        content:
-          'Chào mừng bạn đến với Nihongo365. Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi',
-        targetId: '12345678',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-          nickName: 'Test',
-        },
-        renderTime: true,
-        sendStatus: 0,
-        time: '1542006036549',
-        isIPhoneX: isIPX,
+export const Chat = props => {
+  const defaultMessages = [
+    {
+      id: uuidv4(),
+      type: 'text',
+      content:
+        'Chào mừng bạn đến với Nihongo365. Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi',
+      targetId: '12345678',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
+        nickName: 'Test',
       },
-      {
-        id: uuidv4(),
-        type: 'text',
-        content: 'hi/{se}',
-        targetId: '12345678',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-          nickName: 'Test',
-        },
-        renderTime: true,
-        sendStatus: 0,
-        time: '1542106036549',
-        isIPhoneX: isIPX,
+      renderTime: true,
+      sendStatus: 0,
+      time: '1542006036549',
+      isIPhoneX: isIPX,
+    },
+    {
+      id: uuidv4(),
+      type: 'text',
+      content: 'hi/{se}',
+      targetId: '12345678',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
+        nickName: 'Test',
       },
-      {
-        id: uuidv4(),
-        type: 'image',
-        content: {
-          uri:
-            'https://upload-images.jianshu.io/upload_images/11942126-044bd33212dcbfb8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
-          width: 100,
-          height: 80,
-        },
-        targetId: '12345678',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-          nickName: 'Test',
-        },
-        renderTime: false,
-        sendStatus: 0,
-        time: '1542106037000',
-        isIPhoneX: isIPX,
+      renderTime: true,
+      sendStatus: 0,
+      time: '1542106036549',
+      isIPhoneX: isIPX,
+    },
+    {
+      id: uuidv4(),
+      type: 'image',
+      content: {
+        uri:
+          'https://upload-images.jianshu.io/upload_images/11942126-044bd33212dcbfb8.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+        width: 100,
+        height: 80,
       },
-      {
-        id: uuidv4(),
-        type: 'text',
-        content: 'Hi Admin/{weixiao}',
-        targetId: '88886666',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-        },
-        renderTime: true,
-        sendStatus: -2,
-        time: '1542177036549',
-        isIPhoneX: isIPX,
+      targetId: '12345678',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
+        nickName: 'Test',
       },
-      {
-        id: uuidv4(),
-        type: 'voice',
-        content: {
-          uri:
-            'http://m10.music.126.net/20190810141311/78bf2f6e1080052bc0259afa91cf030d/ymusic/d60e/d53a/a031/1578f4093912b3c1f41a0bfd6c10115d.mp3',
-          length: 10,
-        },
-        targetId: '12345678',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-          nickName: 'Test',
-        },
-        renderTime: true,
-        sendStatus: 1,
-        time: '1542260667161',
-        isIPhoneX: isIPX,
+      renderTime: false,
+      sendStatus: 0,
+      time: '1542106037000',
+      isIPhoneX: isIPX,
+    },
+    {
+      id: uuidv4(),
+      type: 'text',
+      content: 'Hi Admin/{weixiao}',
+      targetId: '88886666',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
       },
-      {
-        id: uuidv4(),
-        type: 'voice',
-        content: {
-          uri:
-            'http://m10.music.126.net/20190810141311/78bf2f6e1080052bc0259afa91cf030d/ymusic/d60e/d53a/a031/1578f4093912b3c1f41a0bfd6c10115d.mp3',
-          length: 30,
-        },
-        targetId: '88886666',
-        chatInfo: {
-          avatar: require('app/assets/logo.png'),
-          id: '12345678',
-        },
-        renderTime: true,
-        sendStatus: 1,
-        time: '1542264667161',
-        isIPhoneX: isIPX,
+      renderTime: true,
+      sendStatus: -2,
+      time: '1542177036549',
+      isIPhoneX: isIPX,
+    },
+    {
+      id: uuidv4(),
+      type: 'voice',
+      content: {
+        uri:
+          'http://m10.music.126.net/20190810141311/78bf2f6e1080052bc0259afa91cf030d/ymusic/d60e/d53a/a031/1578f4093912b3c1f41a0bfd6c10115d.mp3',
+        length: 10,
       },
-    ],
-    // chatBg: require('../../source/bg.jpg'),
-    inverted: false, // require
-    voiceHandle: true,
-    currentTime: 0,
-    recording: false,
-    paused: false,
-    stoppedRecording: false,
-    finished: false,
-    audioPath: '',
-    voicePlaying: false,
-    voiceLoading: false,
-  };
-
-  sendMessage = (type, content, isInverted) => {
+      targetId: '12345678',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
+        nickName: 'Test',
+      },
+      renderTime: true,
+      sendStatus: 1,
+      time: '1542260667161',
+      isIPhoneX: isIPX,
+    },
+    {
+      id: uuidv4(),
+      type: 'voice',
+      content: {
+        uri:
+          'http://m10.music.126.net/20190810141311/78bf2f6e1080052bc0259afa91cf030d/ymusic/d60e/d53a/a031/1578f4093912b3c1f41a0bfd6c10115d.mp3',
+        length: 30,
+      },
+      targetId: '88886666',
+      chatInfo: {
+        avatar: require('app/assets/logo.png'),
+        id: '12345678',
+      },
+      renderTime: true,
+      sendStatus: 1,
+      time: '1542264667161',
+      isIPhoneX: isIPX,
+    },
+  ];
+  const [messages, setMessages] = useState(defaultMessages);
+  const sendMessage = (type, content, isInverted) => {
     // console.log(type, content, isInverted);
     const newMsg = {
       id: uuidv4(),
@@ -153,21 +137,18 @@ export class Chat extends React.Component {
       time: Date.now() + '',
       isIPhoneX: isIPX,
     };
-    const newMessagesList = [...this.state.messages, newMsg];
-    this.setState({ ...this.state, messages: newMessagesList });
+    setMessages([...messages, newMsg]);
   };
 
-  render() {
-    return (
-      <ChatScreen
-        ref={e => (this.chat = e)}
-        messageList={this.state.messages}
-        sendMessage={this.sendMessage}
-        placeholder="Nhập tin nhắn..."
-        useEmoji={false}
-        useVoice={false}
-        usePlus={false}
-      />
-    );
-  }
-}
+  return (
+    <ChatScreen
+      // ref={e => (this.chat = e)}
+      messageList={messages}
+      sendMessage={sendMessage}
+      placeholder="Nhập tin nhắn..."
+      useEmoji={false}
+      useVoice={false}
+      usePlus={false}
+    />
+  );
+};

9fd295dc0b1ee4a5c2b1aa5ba4b47ddda544f2d6
seta-nathan <nnguyen4@veritone.com>
2021-09-10 03:38:08 -0700

    account-screen: add payment info

---
+++ app/constants/payment.constants.js
@@ -0,0 +1,25 @@
+export const SERVICE = {
+  N5: { price: 0, label: 'N5' },
+  N4: { price: 200000, label: 'N4' },
+  N3: { price: 350000, label: 'N3' },
+  N2: { price: 500000, label: 'N2' },
+  N1: { price: 1000000, label: 'N1' },
+  VIP: { price: 2000000, label: 'VIP' },
+  REMOVE_ADS: { price: 50000, label: 'Bỏ quảng cáo', expirable: true },
+};
+
+export const TARGET_BANK_ACCOUNT = {
+  TPBANK: {
+    bankName: 'Ngân hàng TMCP Tiên Phong (TPB)',
+    branch: 'TPbank Mỹ Đình',
+    ownerAccount: '00334981402',
+    ownerName: 'THEN VAN DUC',
+    ownerPhone: '0379269357',
+    ownerEmail: 'adm.nihongo365@gmail.com',
+  },
+};
+
+export const OWNER_INFO = {
+  ownerPhone: '0379269357',
+  ownerEmail: 'adm.nihongo365@gmail.com',
+};
